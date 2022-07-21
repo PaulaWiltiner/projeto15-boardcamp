99e730999d08abfc05ab0e13ac86a894e41c3076
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-07-21 17:22:59 -0300

    games

---
+++ src/routes/gamesRouter.js
@@ -0,0 +1,9 @@
+import { postGames, getGames } from "../controllers/categoriesController.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/games", postGames);
+router.get("/games", getGames);
+
+export default router;
