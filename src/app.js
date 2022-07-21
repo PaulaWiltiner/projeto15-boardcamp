bd7984c4d6399205686ef55e01ff949b8dd3af2f
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-07-21 16:49:38 -0300

    add env.example

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import productsRouter from "./routes/categoriesRouter.js";
+import categoriesRouter from "./routes/categoriesRouter.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-app.use(productsRouter);
+app.use(categoriesRouter);
 
 app.listen(process.env.PORT);
